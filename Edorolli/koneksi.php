4b34d63574c44560ccd3d6db0e7b97076dd1eaf7
erwin <erwinhariadinata.mipa3@gmail.com>
2024-05-13 09:58:52 +0800

    bokon

---
+++ Edorolli/koneksi.php
@@ -0,0 +1,26 @@
+<?php
+$koneksi = mysqli_connect('localhost', 'root', '', 'project-pweb');
+
+// Periksa koneksi
+if (!$koneksi) {
+    die("Koneksi database gagal: " . mysqli_connect_error());
+}
+
+// Mendapatkan data dari form
+$gmail = $_POST['gmail'];
+$password = $_POST['password'];
+
+// Eksekusi query
+$query = mysqli_query($koneksi, "SELECT * FROM user WHERE gmail='$gmail' AND password='$password'");
+if (!$query) {
+    die(mysqli_error($koneksi));
+}
+
+// Memeriksa jumlah baris yang ditemukan
+$cek = mysqli_num_rows($query);
+
+if ($cek == 0) {
+    echo "Password atau Username Salah!";
+} else {
+    echo "Berhasil!";
+}
