7bfdf2966e382095dc612ac7974466fc17694a95
erwin <erwinhariadinata.mipa3@gmail.com>
2024-05-15 06:09:53 +0800

    c

--- Edorolli/contact_submit.js
+++ Edorolli/contact_submit.js
@@ -1,21 +1,22 @@
 // Mengecek URL parameter untuk status
 const urlParams = new URLSearchParams(window.location.search);
-const status = urlParams.get('status');
-const popup = document.getElementById('popup');
+const status = urlParams.get("status");
+const popup = document.getElementById("popup");
 
-if (status === 'sukses') {
-    popup.textContent = 'Terimakasih! Pesan anda telah terkirim^^';
-    popup.classList.add('sukses');
-    popup.style.visibility = 'visible';
-} else if (status === 'gagal') {
-    popup.textContent = 'Maaf, pesan anda tidak terkirim. Silahkan mengirimkan kembali';
-    popup.classList.add('gagal');
-    popup.style.visibility = 'visible';
+if (status === "sukses") {
+  popup.textContent = "Terimakasih! Pesan anda telah terkirim^^";
+  popup.classList.add("sukses", "visible");
+  popup.style.visibility = "visible";
+} else if (status === "gagal") {
+  popup.textContent =
+    "Maaf, pesan anda tidak terkirim. Silahkan mengirimkan kembali";
+  popup.classList.add("gagal", "visible");
+  popup.style.visibility = "visible";
 }
 
 // Menghilangkan popup setelah 5 detik
 if (status) {
-    setTimeout(() => {
-        popup.style.visibility = 'hidden';
-    }, 5000);
+  setTimeout(() => {
+    popup.style.visibility = "hidden";
+  }, 5000);
 }
