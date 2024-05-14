1b362324310c1be9def459154ac562e44a4cfd03
erwin <erwinhariadinata.mipa3@gmail.com>
2024-05-15 05:52:34 +0800

    progres 15 mei 5.52

--- Edorolli/contact_submit.js
+++ Edorolli/contact_submit.js
@@ -1,14 +1,15 @@
+// Mengecek URL parameter untuk status
 const urlParams = new URLSearchParams(window.location.search);
 const status = urlParams.get('status');
 const popup = document.getElementById('popup');
 
 if (status === 'success') {
     popup.textContent = 'Terimakasih! Pesan anda telah terkirim^^';
-    popup.classList.add('success');
+    popup.classList.add('success', 'visible');
     popup.style.visibility = 'visible';
 } else if (status === 'error') {
     popup.textContent = 'Maaf, pesan anda tidak terkirim. Silahkan mengirimkan kembali';
-    popup.classList.add('error');
+    popup.classList.add('error', 'visible');
     popup.style.visibility = 'visible';
 }
 
@@ -16,5 +17,6 @@ if (status === 'success') {
 if (status) {
     setTimeout(() => {
         popup.style.visibility = 'hidden';
+        popup.classList.remove('visible');
     }, 5000);
 }
