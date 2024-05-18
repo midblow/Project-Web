ccc564df6ef044201392f6367373721ac295d9af
erwin <erwinhariadinata.mipa3@gmail.com>
2024-05-18 19:48:10 +0800

    cek

--- Edorolli/js/home_login.js
+++ Edorolli/js/home_login.js
@@ -13,6 +13,17 @@ $(document).ready(function () {
     margin: 10,
     nav: true,
     dots: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
   });
 
   document.querySelectorAll(".nav-item").forEach((item) => {
