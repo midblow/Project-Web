55258db1fe8f9bca841ed1721c1302d46b651dd4
erwin <erwinhariadinata.mipa3@gmail.com>
2024-06-04 01:46:08 +0800

    TESTING!!

--- Edorolli/Admin/content_venue.php
+++ Edorolli/Admin/content_venue.php
@@ -1,7 +1,7 @@
     <?php
     session_start();
 
-    if (!isset($_SESSION['name'])) {
+    if (!isset($_SESSION['username'])) {
     header("Location: http://localhost/Project-Web/Edorolli/login_user.php");
     exit();
     }
